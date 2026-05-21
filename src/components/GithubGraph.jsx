df51fa10c614a4358abb4859528d55c4efae8435
AngadBari <angadbari2@gmail.com>
2026-05-21 21:17:02 +0530

    ...

--- src/components/GithubGraph.jsx
+++ src/components/GithubGraph.jsx
@@ -5,10 +5,7 @@ import { GitHubCalendar } from "react-github-calendar";
 const GithubGraph = () => {
   return (
     <div className="overflow-x-auto">
-      <GitHubCalendar
-        username="AngadBari"
-        colorScheme="dark"
-      />
+      <GitHubCalendar username="AngadBari" colorScheme="dark" />
     </div>
   );
 };
